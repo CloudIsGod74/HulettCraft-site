608d9abe8a208aceb1a482f20a985d0c7149d84f
Topher Howell <thowell7491@outlook.com>
2026-01-24 23:31:19 -0600

    Auto update Sat 01/24/2026 23:31:18.97

--- src/pages/api/admin/auth/callback.ts
+++ src/pages/api/admin/auth/callback.ts
@@ -1,45 +1,5 @@
-import { ADMIN_USERS } from "../../../../lib/adminAllowlist";
-
-export async function GET({ url, locals }: { url: URL; locals: any }) {
-  const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = locals.runtime.env;
-
-  const code = url.searchParams.get("code");
-  if (!code) {
-    return new Response("NO CODE RECEIVED", { status: 400 });
-  }
-
-  const tokenRes = await fetch("https://github.com/login/oauth/access_token", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      client_id: GITHUB_CLIENT_ID,
-      client_secret: GITHUB_CLIENT_SECRET,
-      code,
-    }),
+export async function GET() {
+  return new Response("CALLBACK ALIVE", {
+    headers: { "Content-Type": "text/plain" },
   });
-
-  const tokenData = await tokenRes.json();
-  if (!tokenData.access_token) {
-    return new Response("TOKEN EXCHANGE FAILED", { status: 500 });
-  }
-
-  const userRes = await fetch("https://api.github.com/user", {
-    headers: {
-      Authorization: `Bearer ${tokenData.access_token}`,
-      Accept: "application/json",
-    },
-  });
-
-  const user = await userRes.json();
-  const login = String(user.login).toLowerCase();
-
-  const authorized = ADMIN_USERS.has(login);
-
-  return new Response(
-    `GITHUB LOGIN: ${user.login}\nAUTHORIZED: ${authorized}`,
-    { headers: { "Content-Type": "text/plain" } }
-  );
 }
